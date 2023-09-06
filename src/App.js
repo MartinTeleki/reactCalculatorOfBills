3751bc7a96721adb102b29c93f003b66e691f112
MartinTeleki <Martinteleki@seznam.cz>
2023-09-07 01:14:59 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,200 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./style.css";
 
 function App() {
+  const [billAmount, setBillAmount] = useState("");
+  const [billService, setBillService] = useState(0);
+  const [billFriendService, setBillFriendService] = useState(0);
+  //console.log(billService);
+
+  const handleInputChange = (e) => {
+    const inputValue = e.target.value;
+    setBillAmount(inputValue);
+    //console.log(inputValue);
+  };
+
+  const handleOptionChangeService = (e) => {
+    const optionValue = e.target.value;
+    setBillService(parseInt(optionValue));
+    // console.log(optionValue);
+  };
+
+  const handleOptionChangeFriend = (e) => {
+    const optionValue = e.target.value;
+    setBillFriendService(parseInt(optionValue));
+    // console.log(optionValue);
+  };
+
+  const reset = () => {
+    setBillAmount("");
+    setBillService(0);
+    setBillFriendService(0);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form
+        billAmount={billAmount}
+        handleInputChange={handleInputChange}
+        billService={billService}
+        handleOptionChangeService={handleOptionChangeService}
+        billFriendService={billFriendService}
+        handleOptionChangeFriend={handleOptionChangeFriend}
+      />
+      <Summary
+        billAmount={billAmount}
+        billService={billService}
+        billFriendService={billFriendService}
+        reset={reset}
+      />
+    </div>
+  );
+}
+
+function Form({
+  billAmount,
+  handleInputChange,
+  billService,
+  handleOptionChangeService,
+  billFriendService,
+  handleOptionChangeFriend,
+}) {
+  //console.log(billService);
+
+  return (
+    <div>
+      <Bill billAmount={billAmount} handleInputChange={handleInputChange} />
+      <Service
+        billAmount={billAmount}
+        billService={billService}
+        handleOptionChangeService={handleOptionChangeService}
+        handleInputChange={handleInputChange}
+      />
+      <FriendService
+        billAmount={billAmount}
+        billFriendService={billFriendService}
+        handleOptionChangeFriend={handleOptionChangeFriend}
+      />
+    </div>
+  );
+}
+
+function Bill({ billAmount, handleInputChange, billService }) {
+  return (
+    <div>
+      <p>
+        How much was the bill?{" "}
+        <input type="number" value={billAmount} onChange={handleInputChange} />
+      </p>
+    </div>
+  );
+}
+
+function Service({ handleOptionChangeService, billService }) {
+  // console.log(billService);
+  return (
+    <OptionsServis
+      billService={billService}
+      handleOptionChangeService={handleOptionChangeService}
+    />
+  );
+}
+
+function FriendService({ handleOptionChangeFriend, billFriendService }) {
+  //console.log(billFriendService);
+  return (
+    <OptionsFriend
+      billFriendService={billFriendService}
+      handleOptionChangeFriend={handleOptionChangeFriend}
+    />
+  );
+}
+
+function OptionsServis({ handleOptionChangeService, billService }) {
+  const serviceOptions = {
+    0: "Terrible 0%",
+    5: "Good 5%",
+    10: "Fantastic 10%",
+    20: "Wonderful 20%",
+  };
+
+  const renderOptions = () => {
+    const options = [0, 5, 10, 20];
+
+    return options.map((option) => (
+      <Option key={option} value={option} selected={billService === option}>
+        {serviceOptions[option]}
+      </Option>
+    ));
+  };
+
+  return (
+    <>
+      <p>
+        How do you like the service?{" "}
+        <select onChange={handleOptionChangeService}>{renderOptions()}</select>
+      </p>
+    </>
+  );
+}
+
+function OptionsFriend({ handleOptionChangeFriend, billFriendService }) {
+  const friendOptions = {
+    0: "Terrible 0%",
+    5: "Good 5%",
+    10: "Fantastic 10%",
+    20: "Wonderful 20%",
+  };
+
+  const renderOptions = () => {
+    const options = [0, 5, 10, 20];
+
+    return options.map((option) => (
+      <Option
+        key={option}
+        value={option}
+        selected={billFriendService === option}
+      >
+        {friendOptions[option]}
+      </Option>
+    ));
+  };
+
+  return (
+    <>
+      <p>
+        How does your friend like the service?{" "}
+        <select onChange={handleOptionChangeFriend}>{renderOptions()}</select>
+      </p>
+    </>
+  );
+}
+
+function Option({ value, selected, children }) {
+  return (
+    <option value={value} selected={selected}>
+      {children}
+    </option>
+  );
+}
+
+function Summary({ billAmount, billService, billFriendService, reset }) {
+  const totalAmount = parseFloat(billAmount);
+  const serviceTip = parseFloat((totalAmount * billService) / 100);
+  const friendTip = (totalAmount * billFriendService) / 100;
+  const totalBill = totalAmount + serviceTip + friendTip;
+
+  return (
+    <div>
+      <p>
+        You pay {isNaN(billAmount) ? 0 : billAmount} ($
+        {billAmount} + ${isNaN(serviceTip) ? 0 : serviceTip} tip for service + $
+        {isNaN(friendTip) ? 0 : friendTip} tip for your friend. And together u
+        have paid ${isNaN(totalBill) ? 0 : totalBill})
+      </p>
+      <button type="button" onClick={reset}>
+        Reset
+      </button>
     </div>
   );
 }
